e0387f92ec8eb21d5719a117b3f9fa56f641a458
JonGudenzi <jongudenzi@gmail.com>
2020-12-05 13:50:12 -0500

    second api working. image and api data in card

--- assets/script.js
+++ assets/script.js
@@ -7,9 +7,8 @@ $(".submit").on("click", function () {
     let inputUserDate = $(".inputValue").val();
     inputDate(inputUserDate);
 });
-
+// first api
 function inputDate (userDate) {
-    
 const settings = {
 	"async": true,
 	"crossDomain": true,
@@ -23,16 +22,39 @@ const settings = {
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
+	// let imageEl = $("<img src = " + response.content[1].image + ">");
 	userSongName = response.content[1].title;
     userArtistName = response.content[1].artist;
     artistDate = response.info.date;
 
-    let billBoardDiv = $("<div>");
-    billBoardDiv.append(userArtistName, artistDate, userSongName);
-    $(".billBoardEl").append(billBoardDiv);
+    $(".card-content").append(userArtistName, userSongName, artistDate);
+	// $(".card-image").append(imageEl);
 
-	console.log(response);
+	renderVideoLink(userSongName, userArtistName);
 });
+}
+// second api         
+  function renderVideoLink () {
+	const settingsTwo = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://theaudiodb.p.rapidapi.com/searchtrack.php?t=" + userSongName + "&s=" + userArtistName,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "07be92493emshea6401a64571b97p134b1ejsn07c4838e18d4",
+			"x-rapidapi-host": "theaudiodb.p.rapidapi.com"
+		}
+	};
+	
+	$.ajax(settingsTwo).done(function (responseTwo) {
+		console.log(responseTwo);
+
+		imageEl = $("<img src=" + responseTwo.track[0].strTrackThumb + ">");
+		videoEl = $("<a href=" + responseTwo.track[0].strMusicVid + ">");
+		$(".card-image").append(strTrackThumb);
+		$(".vidlink").append(videoEl);
+
+	});
 }
 
 // Datepicker
@@ -43,23 +65,3 @@ $( function() {
 		dateFormat: "yy-mm-dd"
 	  });
   } );
-                                            
-  
-
- 
-  $.ajax({
-    type: "GET",
-    data: {
-        apikey:"7df083a8630c2141e251e825c85539f1",
-        q_artist: artistSearch,
-        format:"jsonp",
-        callback:"jsonp_callback"
-    },
-    url: "https://api.musixmatch.com/ws/1.1/track.search",
-    dataType: "jsonp",
-    jsonpCallback: 'jsonp_callback',
-    contentType: 'application/json',
-    success: function(data) {
-        console.log(data); 
-  
-
